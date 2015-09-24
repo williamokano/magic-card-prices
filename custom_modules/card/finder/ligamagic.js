02d0014e144303b3db9ae8767636e78e328c745b
williamokano <williamokano@gmail.com>
2015-09-24 20:17:43 -0300

    Updated the ligamagic resource

--- custom_modules/card/finder/ligamagic.js
+++ custom_modules/card/finder/ligamagic.js
@@ -26,12 +26,14 @@ module.exports = {
           //read the data
           var $ = cheerio.load(body);
           var title = $(".cardTitle").text();
-          var card_sets = {};
+          var card_sets_obj = {};
+          var card_set_array = [];
           var card = {};
           $(".overview td").each(function(i, elem) {
             var card_set = $("a", elem).attr("href");
             card_set = card_set.split("%3d")[1];
-            card_sets[card_set] = []; //No prices found yet!
+            card_sets_obj[card_set] = {}; //No prices found yet!
+            card_sets_obj[card_set]["USD"] = []; //Set prices as USD
 
             //@TODO:  Stop reading from the Javascript at the end of the page
             //        and scrappe the page instead. Dumbass! There's a table
@@ -40,21 +42,23 @@ module.exports = {
             //Find the minor price for the card
             var regexMinorPrice = new RegExp("VETprecoMenor\\[" + i.toString() + "\\]\\s+=\\s+\"(.*)\";", "i");
             var minorPrice = body.match(regexMinorPrice)[1].replace(',', '.');
-            card_sets[card_set].push(minorPrice);
+            card_sets_obj[card_set]["USD"].push(minorPrice);
 
             //Find the medium price for the card
             var regexMediumPrice = new RegExp("VETprecoMedio\\[" + i.toString() + "\\]\\s+=\\s+\"(.*)\";", "i");
             var mediumPrice = body.match(regexMediumPrice)[1].replace(',', '.');
-            card_sets[card_set].push(mediumPrice);
+            card_sets_obj[card_set]["USD"].push(mediumPrice);
 
             //Find the major price for the card
             var regexMajorPrice = new RegExp("VETprecoMaior\\[" + i.toString() + "\\]\\s+=\\s+\"(.*)\";", "i");
             var majorPrice = body.match(regexMajorPrice)[1].replace(',', '.');
-            card_sets[card_set].push(majorPrice);
+            card_sets_obj[card_set]["USD"].push(majorPrice);
           });
 
           card["title"] = title;
-          card["sets"] = card_sets;
+          card["prices"] = card_sets_obj;
+          card["sets"] = Object.keys(card_sets_obj);
+          card["currencies"] = ["USD"];
           card["url"] = request_options["url"];
 
           resolve(card);
