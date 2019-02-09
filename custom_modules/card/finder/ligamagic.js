0bb56223e9f8769742140933e6096293c24abadc
tdopires <tdopires@gmail.com>
2019-02-09 16:03:39 -0200

    Fixing card fetch (some card names returned more than one result on search)

--- custom_modules/card/finder/ligamagic.js
+++ custom_modules/card/finder/ligamagic.js
@@ -15,7 +15,7 @@ module.exports = {
           'User-Agent' : 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36',
           'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
         },
-        url: "http://ligamagic.com.br/?view=cards%2Fsearch&card=" + cardname
+        url: "https://ligamagic.com.br/?view=cards/card&card=" + cardname
       };
       requestp(request_options)
         .then(function(body) {
